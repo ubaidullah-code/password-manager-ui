697f0dcbf4d5607d552fbb705045ca13f18d185d
ubaidullah-code <ubaidque14@gmail.com>
2025-09-04 23:10:26 +0500

    one

--- src/config/api.js
+++ src/config/api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://password-manager-backend-kappa.vercel.app/",
   withCredentials: true, // if you are using cookies / sessions
 });
 export default api;
